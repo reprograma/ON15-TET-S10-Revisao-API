dc45bf434d2c462d558528532e17627a03f1692f
elizapimentel <elizapimentel@hotmail.com>
2022-06-03 20:16:47 -0300

    subindo atividade da semana 10

--- paraolar/src/controllers/businessControllers.js
+++ paraolar/src/controllers/businessControllers.js
@@ -41,13 +41,20 @@ const findSome = (req,res) => {
             filterStores = filterStores.filter(business => business.store.toLowerCase().normalize("NFD").replace(/[^a-zA-Zs]/g, "").includes(store.toLowerCase().normalize("NFD").replace(/[^a-zA-Zs]/g, "")))
             
         }
-
+   
         if(type) {
             filterStores = filterStores.filter(storeType => storeType.type.toString().toLowerCase().includes(type.toLowerCase()))
         }
 
         if(neighborhood) {
             filterStores = filterStores.filter(storeNeighborhood => storeNeighborhood.neighborhood.toLocaleLowerCase().includes(neighborhood.toLocaleLowerCase()))
+        }
+        if(!store || !type || !neighborhood) {
+            throw {
+                statusCode: 400,
+                message: "Não possuímos estabelecimentos cadastrados nesses parametros",
+                
+            }
         }
 
         if(filterStores.length === 0) {
@@ -64,9 +71,9 @@ const findSome = (req,res) => {
 
 const createStore = (req, res) => {
     //const with items that have to put in the body
-    const { store, type, neighborhood, address, number, paymment, site } = req.body
+    const { likes, dislikes, store, type, neighborhood, address, number, paymment, site } = req.body
     try {
-        const id = businessModel.length
+        const id = (businessModel.length)+1
                   
         if(store === null || store === undefined || store.trim() === "") {
             throw {
@@ -90,15 +97,15 @@ const createStore = (req, res) => {
 
         
         //const with keys that gonna appear in the body
-        const newCommerce = { id, store, type, neighborhood, address, number, paymment, site}        
+        const newCommerce = { id, likes, dislikes, store, type, neighborhood, address, number, paymment, site}        
         businessModel.push(newCommerce)
 
         const objKeys = Object.keys(newCommerce)
+        
         //if doesn't have all keys
         objKeys.forEach(key => {
-            let valide = true
             if(!newCommerce[key] ) {
-                valide = false
+                
                 throw {
                     statusCode: 406,
                     message: "Todos os itens devem ser adicionados para criação de novo estabelecimento",
@@ -114,10 +121,121 @@ const createStore = (req, res) => {
     }
 }
 
+const updateStore = (req, res) => {
+    const { id, likes, dislikes, store, type, neighborhood, address, number, paymment, site } = req.body
+    try {
+        const idRequest = req.params.id
+        const bodyRequest = req.body
+        
+        if(bodyRequest.id != idRequest) throw new Error("Id não confere com solicitação")
+        if(!bodyRequest.store) throw new Error ("Estabelecinemto precisa de nome")
+             
+        const businessUpdated = { id, likes, dislikes, store, type, neighborhood, address, number, paymment, site}       
+
+        businessModel.push(businessUpdated)
+
+        const sameKeys = Object.keys(businessUpdated)
+        
+        sameKeys.forEach(key => {
+            if(!businessUpdated[key]) {
+                throw new Error("Item não pode atualizado, pois não possui todas as chaves")
+            }
+        })        
+
+        res.status(200).json(bodyRequest)
+    } catch (error) {
+        res.status(405).json({
+            details: error.message})
+    }
+
+}
+
+/* const updateLikes = (req, res) => {
+    // const { likes, dislikes } = req.body
+    try {
+        const bodyRequest = req.body       
+        //find the place by the name
+        const idRequest = req.params.id
+        //check if its the same as the one at json
+        const storeFound = businessModel.find(search => search.id.toLocaleLowerCase().includes(idRequest.toLocaleLowerCase()))
+
+        if(!storeFound) throw new Error("Estabelecimento não localizado")
+
+        const index = businessModel.indexOf(storeFound)
+
+        const itsLiked = businessModel.find(store => {
+            if(store.likes == true) {
+                store.dislikes = false
+                itsLiked = businessModel.splice(index, 1, store.dislikes)
+                businessModel.push(itsLiked)
+            }
+        } )
+
+        const itsDisliked = businessModel.find(store => {
+            if(store.dislikes == true) {
+                store.likes = false
+                itsDisliked = businessModel.splice(index, 1, store.likes)
+                businessModel.push(itsDisliked)
+
+            }
+        })
+
+
+        res.status(200).json(bodyRequest)
+    } catch (error) {
+        res.status(404).json({
+            details: error.message})
+        
+    }
+} */
+
+const changeStore = (req, res) => {
+    try {
+        const idRequest = req.params.id 
+        const bodyRequest = req.body 
+
+        
+        const businessFound = businessModel.find(store => store.id == idRequest)        
+        
+        const index = businessModel.indexOf(businessFound)        
+
+        if(bodyRequest.id != idRequest) throw new Error("Id não confere com solicitação")       
+            
+        businessModel.splice(index, 1, bodyRequest)
+        res.status(200). json(bodyRequest)
+        
+    } catch (error) {
+        res.status(404).json({message: error.message}) 
+    }
+
+
+
+}
+
+const deleteStore = (req, res) => {
+    const idRequest = req.params.id
+    const foundStore = businessModel.find(store => store.id == idRequest)
+    
+    const index = businessModel.indexOf(foundStore)
+    
+    businessModel.splice(index,1)
+    
+    if(!foundStore) throw new Error ("Id não incluso no sistema") 
+
+    res.status(200).json([{
+        "mensagem": "Estabelecimento deletado com sucesso",
+        "item-deletado" : foundStore,
+        businessModel 
+    }])
+}
 
 module.exports = {
     getAll,
     getById,
     findSome,
-    createStore
+    createStore,
+    updateStore,
+    // updateLikes,
+    changeStore,
+    deleteStore
 }
--- paraolar/src/routes/businessRoutes.js
+++ paraolar/src/routes/businessRoutes.js
@@ -6,6 +6,10 @@ router.get("/", controller.getAll)
 router.get("/list", controller.findSome)
 router.get("/:id", controller.getById)
 router.post("/add", controller.createStore)
+router.patch("/update/:id", controller.updateStore)
+// router.patch("like/:id", controller.updateLikes)
+router.put("/change/:id", controller.changeStore)
+router.delete("/delete/:id", controller.deleteStore)
 
 
 module.exports = router
