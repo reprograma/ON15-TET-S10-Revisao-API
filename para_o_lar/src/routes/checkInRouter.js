5f64fdbdb63475c6294529f18e9f8b897b6ea5ae
anaelisaq <anaelisa.abs@gmail.com>
2022-06-06 00:36:00 -0300

    atualizando Check In router

--- para_o_lar/src/routes/checkInRouter.js
+++ para_o_lar/src/routes/checkInRouter.js
@@ -3,9 +3,11 @@ const controller = require("../controllers/checkInController")
 const router = express.Router()
 
 router.get("/", controller.pagInicial)
-router.get("/buscar", controller.findAll)
-router.get("/lugar", controller.findOneLocalByName)
-router.get("/:id", controller.findById)
-router.post("/cadastrar", controller.createLocal)
+router.get("/list", controller.findAll)
+router.get("/list/:id", controller.findById)
+router.get("/spotName", controller.findOnePlaceByName)
+router.get("/spotPayment", controller.findByPayment)
+router.post("/register", controller.createPlace)
+router.delete("/delete", controller.deletePlace)
 
 module.exports = router
