4f27bb4077ca8d9750f797a5410da3eaedf64cf4
anaelisaq <anaelisa.abs@gmail.com>
2022-06-09 13:57:24 -0300

    atualizando controllers

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,57 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-const livrosModel = require('../models/livrosModels.json')
-
-const findAllEbooks = (req, res) => {
-    /**
-     * find -> encontrar
-     * All -> todos
-     * Ebooks -> recurso
-     */
-    const { title = null, page = null, autor = null } = req.query
-
-    try {
-        let filterEbooks = livrosModel.slice()
-
-        if (filterEbooks.length===0) {
-            return res.status(200).json({
-                message:  "Ainda não possuimos livros cadastrados em nossa biblioteca"
-            })
-        }
-
-        if (autor) {
-            filterEbooks = filterEbooks.filter(currentEbook => currentEbook
-                .autor
-                .toLocaleLowerCase()
-                .includes(autor.toLocaleLowerCase())
-            )
-        }
-
-        if (title) {
-            filterEbooks = filterEbooks.filter(currentEbook => currentEbook
-                .titulo
-                .toLocaleLowerCase()
-                .includes(title.toLocaleLowerCase())
-            )
-        }
-
-        if (filterEbooks.length === 0) {
-            throw new Error("descupa, mas não foi encontrado nenhum resultado para essa busca")
-        }
-
-        res.status(200).json(filterEbooks)
-
-    } catch (error) {
-        console.error(error)
-        console.log('query recebida: ', req.query)
-
-        res.status(404).json({
-            message: error.message,
-            details: "query invalida: ",
-            query: req.query
-=======
-=======
->>>>>>> 9b159333f70525ae070b449d887ceb666ab80250
 const livrosModel = require("../models/livrosModels.json")
 
 const findAllEbooks = (request, response) => {
@@ -87,54 +33,30 @@ const findAllEbooks = (request, response) => {
             message: error.message,
             details: "Não foi possível encontrar um resultado para essa query :(",
             errorCode: 404
-<<<<<<< HEAD
->>>>>>> 9b15933 (aula 28/05)
-=======
->>>>>>> 9b159333f70525ae070b449d887ceb666ab80250
         })
     }
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-const findById = (req, res) => {
-    const { id } = req.params
-    // const findEbook = livrosModel[id] 
-    try {
-        const findEbook = livrosModel.find(ebook => ebook.id == id)// null | ebook
-
-        if (!findEbook) throw new Error(`desculpa, não foi possivel encontrar o livro com o id ${id}`)
-
-        res.status(200).json(findEbook)
-
-    } catch (error) {
-        console.error(error)
-        res.status(404).json({
-            message: "Poxa, desculpa, foi mal, ainda não possuimos esse livro no nosso catálago.",
-            details: error.message,
-=======
-=======
->>>>>>> 9b159333f70525ae070b449d887ceb666ab80250
 const findById = (request, response) => {
-   try {
-        let idRequest = request.params.id
-        let findEbook = livrosModel.find(livro => livro.id == idRequest)
+    try {
+        let {id} = request.params.id
+        let findEbook = livrosModel.find(livro => livro.id == id)
 
-        if(!findEbook) throw new Error(`desculpa, não foi possível encontrar o id ${idRequest}`)
+        if(!findEbook) throw new Error(`desculpa, não foi possível encontrar o id ${id}`)
 
         response.status(200).json(findEbook)
-   } catch (error){
-       console.error(error)
-       response.status(404).json({
-           message: "Poxa, não temos esse livro no catálogo, foi mal :(",
-           details: error.message,
-           errorCode: 404
-       })
-   }
+        } catch (error){
+            console.error(error)
+            response.status(404).json({
+            message: "Poxa, não temos esse livro no catálogo, foi mal :(",
+            details: error.message,
+            errorCode: 404
+        })
+    }
 }
 
 const findOneEbookByTitle = (request, response) => {
-    const {title} = request.query
+    const {title = "vazio"} = request.query
     try {
         if(!title) throw new Error ("Nenhum parametro inserido para realizar a busca.")
         const findEbook = livrosModel.find(currentEbook => currentEbook.titulo.toLowerCase() == title.toLowerCase())
@@ -150,60 +72,45 @@ const findOneEbookByTitle = (request, response) => {
             message: "Poxa, ainda não temos esse livro no catálogo, foi mal :(",
             details: error.message,
             errorCode: 404
-<<<<<<< HEAD
->>>>>>> 9b15933 (aula 28/05)
-=======
->>>>>>> 9b159333f70525ae070b449d887ceb666ab80250
         })
     }
-}
+}    
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-const findOneEbookByTitle = (req, res) => {
-    // const req.query.title
-    // authorRequest
-    const { title = '"vazio"' } = req.query
+const createEbook = (request, response) => {
+    const { titulo, paginas, autor } = request.body
 
     try {
+        const id = livrosModel.length
+        if (titulo === null || titulo === undefined || titulo.trim() === "") throw {
+            statusCode: 400,
+            message: "Não pode ser criado, poois o título é requerido.",
+            details: "Já existe no sistema um livro com o mesmo título e autor"
 
-        if (!title) throw new Error("Nenhum parametro inserido para relizar a busca")
+        }
+        const findEbookByTitle = livrosModel.find(ebook => ebook.titulo.toLowerCase() == titulo.toLowerCase())
 
-        const findEbook = livrosModel
-            .find(currentEbook => currentEbook.titulo.toLocaleLowerCase() == title.toLocaleLowerCase())
+        if (findEbookByTitle && findEbookByTitle.autor.toLowerCase() == autor.toLowerCase()){
+            throw{
+                statusCode: 409,
+                message: "Já existe um livro com o mesmo titulo e autor",
+                details: "Já existe no sistema um livro com o mesmo título e autor"
+            }
 
-        if (!findEbook) throw new Error(`desculpa, não foi possivel encontrar o livro com o title ${title}`)
+        }
 
-        res.status(200).json(findEbook)
+        const newEbook = {id, titulo, paginas, autor}
+        livrosModel.push(newEbook)
 
+        response.status(201).json(newEbook)
     } catch (error) {
-        console.error(error)
-        res.status(404).json({
-            "message": "desculpa, ainda não possuimos livros com esse titulo",
-            "details": error.message
-        })
+        
     }
+
 }
 
-const createEbook = (req, res) => {
-    const { title, paginas, author} = req.body
-
-    if (!title || title.trim()== "") throw new Error()
-}
-
-module.exports = {
-    findAllEbooks,
-    findById,
-    findOneEbookByTitle
-=======
-=======
->>>>>>> 9b159333f70525ae070b449d887ceb666ab80250
 module.exports = {
     findAllEbooks,
     findById,
     findOneEbookByTitle,
-<<<<<<< HEAD
->>>>>>> 9b15933 (aula 28/05)
-=======
->>>>>>> 9b159333f70525ae070b449d887ceb666ab80250
+    createEbook
 }
