1da1c5e1fcb6f74a2d7ca95637bcebb625a7891b
keyla-3 <keylafcp@gmail.com>
2022-07-22 15:31:30 -0300

    exercicio semana 10

--- para_o_lar/src/app.js
+++ para_o_lar/src/app.js
@@ -6,5 +6,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const rotas = require("./routes/estabelecimentos")
+
+app.use("/estabelecimento", rotas)
+
 
 module.exports = app;   
